86683c6cfd3b703d2d48ea7fa3b90973b95ebb2b
annele <anna.kirillova51@gmail.com>
2020-11-18 23:56:51 +0100

    update for getocations method

--- TravelPlanner/WeatherForToday.cs
+++ TravelPlanner/WeatherForToday.cs
@@ -31,19 +31,31 @@ namespace TravelPlanner
             var s = w.DownloadString($"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={APIKEY}&q={cityname}");
 
             JArray o = JArray.Parse(s);
-            var key = o[0]["Key"].ToString();
+            //var key = o[0]["Key"].ToString();
             var city = o[0]["LocalizedName"].ToString();
-            var country = o[1]["LocalizedName"].ToString();
-            foreach (var bla in o)
-            {
-               // var we = new WeatherForDay();
-                Clouds = o[0]["Key"].ToString();
+            var country = o[0]["Country"]["LocalizedName"].ToString();
+            string location; 
+
+             foreach (var bla in o)
+             {
+                var key = o[0]["Key"].ToString();
+                location = city + " " +country;
+
+                //locationsList.Add(key, location);
             }
-            Console.WriteLine(key, city, country);
+           
 
             return locationsList;
         }
 
+
+        public List<T> GetWeatherForDay(string cityname)
+        {
+            List<T> weatherForDay = new List<T>();
+
+
+            return weatherForDay;
+        }
 
 
         /*   private void button1_Click(object sender, EventArgs e)
